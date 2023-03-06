e727ae09a6a1a22e06b37a927466453e13340b65
Guilherme Aureliano <49173189+GuilhermeAureliano@users.noreply.github.com>
2023-03-06 16:47:47 -0300

    Create hasCycle.java
    Implementação do método hasCycle.

---
+++ hasCycle.java
@@ -0,0 +1,12 @@
+static boolean hasCycle(SinglyLinkedListNode head) {
+        Set<SinglyLinkedListNode> visitados = new HashSet<>();
+        
+        while (head != null) {
+            if (visitados.contains(head)) {
+                return true;
+            }
+            visitados.add(head);
+            head = head.next;
+        }
+        return false;
+}
